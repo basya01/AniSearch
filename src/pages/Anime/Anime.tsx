8558b02dfc54bb9c61e1d207009267d4102615b3
basya01 <arsen.lulukyan@gmail.com>
2022-08-01 12:51:54 +0300

    added anime page

--- src/pages/Anime/Anime.tsx
+++ src/pages/Anime/Anime.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import styles from './Anime.module.scss';
 
 const Anime = () => {
+  const [animeData, setAnimeData] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`https://shikimori.one/api/animes/${id}`).then(({ data }) => {
+      setAnimeData(data);
+    });
+  }, []);
+
   return (
-    <div className='container container__page'>
-      Anime Title
-    </div>
+    <section>
+      <div className="container container__page">
+        <div className={`container__content--anime ${styles.root}`}>
+          {animeData ? (
+            <div className={styles.mainInfo}>
+              <div>
+                <img src={`https://shikimori.one${animeData.image.original}`} alt="anime_image" />
+                <button>Добавить в избранное</button>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+    </section>
   );
 };
 

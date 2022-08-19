03579f392afca978b5e0d96a00872807449ad4c6
basya01 <arsen.lulukyan@gmail.com>
2022-08-19 15:59:47 +0300

    fix videos

--- src/components/Videos/index.tsx
+++ src/components/Videos/index.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 import { Video } from '../../models/Filters';
-import styles from './Videos.module.scss'
+import styles from './Videos.module.scss';
 
 const Videos: FC<{ videos: Video[] }> = ({ videos }) => {
   return (
     <div className={styles.root}>
       {videos.map((item) => (
-        <iframe key={item.id} width="640px" height="360px" src={item.player_url}></iframe>
+        <iframe key={item.id} width="640px" height="360px" src={item.player_url.replace('http', 'https')}></iframe>
       ))}
     </div>
   );

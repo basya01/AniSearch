2a39f067ff24098e404ded277abf9cee8eb0ba4b
basya01 <arsen.lulukyan@gmail.com>
2022-08-24 13:59:46 +0300

    add saving theme in localstorage

--- src/redux/slices/theme.ts
+++ src/redux/slices/theme.ts
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const getTheme = () => {
-  // const theme = `${window.localStorage.getItem('theme')}`;
-  // if (theme === 'light' || theme === 'dark') return theme;
+  const theme = `${window.localStorage.getItem('theme')}`;
+  if (theme === 'light' || theme === 'dark') return theme;
 
   const userMedia = window.matchMedia('(prefers-color-scheme: dark)');
   if (userMedia.matches) return 'dark';
